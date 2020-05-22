8d97cf3afad74afe2853b762e4086ba62ddc96c2
Ken <luozhirong@gmail.com>
2020-05-21 17:04:08 -0700

    makes rot cipher private

--- lib/rot13_recursive.ex
+++ lib/rot13_recursive.ex
@@ -7,17 +7,17 @@ defmodule Rot13Recursive do
     |> rot_cipher("")
   end
 
-  def rot_cipher(<<head, tail::binary>>, acc)
+  defp rot_cipher(<<head, tail::binary>>, acc)
       when (head >= ?A and head <= ?M) or (head >= ?a and head <= ?m),
       do: rot_cipher(tail, acc <> <<head + 13>>)
 
-  def rot_cipher(<<head, tail::binary>>, acc)
+  defp rot_cipher(<<head, tail::binary>>, acc)
       when (head >= ?N and head <= ?Z) or (head >= ?m and head <= ?z),
       do: rot_cipher(tail, acc <> <<head - 13>>)
 
-  def rot_cipher(<<head, tail::binary>>, acc), do: rot_cipher(tail, acc <> <<head>>)
+  defp rot_cipher(<<head, tail::binary>>, acc), do: rot_cipher(tail, acc <> <<head>>)
 
-  def rot_cipher("", acc) do
+  defp rot_cipher("", acc) do
     acc
   end
 end
